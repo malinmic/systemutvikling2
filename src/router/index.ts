c09e2f65de6f33672753fd32b9cc050427b829e4
tobakken <tobakken@stud.ntnu.no>
2022-04-20 13:11:12 +0200

    Rebuilt frontend project without vite

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import { createRouter, createWebHistory, Router, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router: Router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
