5765732516a08814845eda4b750ecd0202c84564
Kristian Valset Aars <krisvaa@stud.ntnu.no>
2022-04-22 12:36:10 +0200

    Create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
+    <!-- This is the recommended layout according to the docs. -->
     <v-app>
+        <v-row>
+            <v-col cols="4">
+                <NavigationDrawerComponent />
+            </v-col>
+        </v-row>
+
         <v-main>
-            <router-view />
+            <v-container fluid>
+                <!-- Show whatever router view is currently active. -->
+                <router-view />
+            </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import NavigationDrawerComponent from "@/components/navbar/NavigationDrawerComponent.vue"
 
 export default defineComponent({
     name: "App",
-
+    components: { NavigationDrawerComponent },
     data() {
         return {
             //
---
+++ src/types/IfcUserAccountInterface.ts
@@ -0,0 +1,7 @@
+export interface UserAccount {
+    firstName: String
+    surname: String
+    email: String
+    phoneNumber: String
+    avatarImagePath?: String
+}
