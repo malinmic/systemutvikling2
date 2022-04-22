f213240756efa58362b3521db0aecf926208cf1c
Vilde Gylterud <vildegy@stud.ntnu.no>
2022-04-22 15:17:08 +0200

    made landingpage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,28 @@
 /* eslint-disable */
-import { createRouter, createWebHistory, Router, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import CreateUserView from "../views/CreateUserView.vue";
+import {
+    createRouter,
+    createWebHistory,
+    Router,
+    RouteRecordRaw,
+} from "vue-router"
+import LandingPage from "../views/LandingPage.vue"
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
+    {
+        path: "/",
+        name: "landingpage",
+        component: LandingPage,
+    },
+    {
     path: "/createUser",
     name: "createUser",
     component: CreateUserView,
   },
-];
+]
 
 const router: Router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+})
 
-export default router;
+export default router
