2f51f1bc9e29d46fc20b82d12010051ebfb7501d
Tor-Øyvind Paulsrud Bakken <tobakken@stud.ntnu.no>
2022-04-22 14:47:28 +0200

    feat(user): Create user feature implemented

--- src/services/api/urls.ts
+++ src/services/api/urls.ts
@@ -1,2 +1,3 @@
 export const API_URL = "http://localhost:8888"
 export const AUTH_URL = API_URL + "/auth"
+export const USER_URL = API_URL + "/user"
---
+++ src/views/CreateUserView.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <h3 class="text-h3 text-center">Registrer Bruker</h3>
+            </v-col>
+            <v-col>
+                <create-user-component />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+// Components
+import CreateUserComponent from "@/components/CreateUserComponent.vue"
+
+export default defineComponent({
+    name: "CreateUserView",
+
+    components: {
+        CreateUserComponent,
+    },
+})
+</script>
