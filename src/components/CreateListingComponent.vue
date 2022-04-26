0c9318ef5cc9990eca11ac7fb4069a9cad185bc2
Tor-Øyvind Paulsrud Bakken <tobakken@stud.ntnu.no>
2022-04-26 12:43:49 +0200

    Feat(listing): Added create listing component

---
+++ src/components/CreateListingComponent.vue
@@ -0,0 +1,134 @@
+<template>
+    <v-form @submit.prevent="submit">
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    <v-text-field
+                        label="Tittel"
+                        variant="outlined"
+                        v-model="title"
+                        type="text"
+                        :error-messages="errors.title"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <v-switch
+                        v-model="isFree"
+                        :label="priceSwitchText"
+                        @click="changePriceLabel()"
+                        class="mr-5"
+                    ></v-switch>
+                </v-col>
+                <v-col cols="12" sm="6" v-if="isFree">
+                    <v-text-field
+                        label="Pris"
+                        variant="outlined"
+                        v-model="price"
+                        type="number"
+                        :error-messages="errors.price"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <v-switch
+                        v-model="showPhone"
+                        :label="phoneSwitchText"
+                        @click="changePhoneLabel()"
+                    ></v-switch>
+                </v-col>
+                <v-col cols="12" sm="6" v-if="showPhone">
+                    <v-text-field
+                        label="Mobilnummer"
+                        variant="outlined"
+                        v-model="phonenumber"
+                        :error-messages="errors.phonenumber"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        label="Postnummer"
+                        variant="outlined"
+                        v-model="address"
+                        :error-messages="errors.address"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-textarea
+                        label="Beskrivelse"
+                        variant="outlined"
+                        v-model="description"
+                        :error-messages="errors.description"
+                    ></v-textarea>
+                </v-col>
+                <v-col class="justify-center d-flex" cols="12">
+                    <v-btn color="#004aad" class="text-white" type="submit"
+                        >Publiser</v-btn
+                    >
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script setup lang="ts">
+import { postListing } from "@/services/api/listing"
+import { useField, useForm } from "vee-validate"
+import { ref } from "vue"
+import { number, object, string } from "yup"
+
+const isFree = ref(false)
+const showPhone = ref(false)
+const priceSwitchText = ref("Gratis")
+const phoneSwitchText = ref("Vis telefonnummer")
+
+const changePriceLabel = () => {
+    if (isFree.value) {
+        priceSwitchText.value = "Gratis"
+    } else {
+        priceSwitchText.value = "Ikke gratis, skriv inn pris for leie: "
+    }
+}
+
+const changePhoneLabel = () => {
+    if (showPhone.value) {
+        phoneSwitchText.value = "Skjul telefonnummer"
+    } else {
+        phoneSwitchText.value = "Vis telefonnummer, venligst skriv inn:"
+    }
+}
+
+const validationSchema = object({
+    title: string().required("Dette feltet er påkrevd"),
+    price: number().nullable(),
+    phonenumber: string().nullable(),
+    description: string().nullable(),
+    address: string().required(),
+})
+
+const { handleSubmit, errors } = useForm({
+    validationSchema,
+})
+
+const { value: title } = useField<string>("title")
+const { value: price } = useField<number>("price")
+const { value: phonenumber } = useField<string>("phonenumber")
+const { value: description } = useField<string>("description")
+const { value: address } = useField<string>("address")
+
+const submit = handleSubmit((values) => {
+    if (!isFree.value) price.value = 0
+    if (!showPhone.value) phonenumber.value = ""
+    if (values.description == undefined) description.value = ""
+    console.log(
+        `Submit ${values.title} ${phonenumber.value} ${price.value} ${description.value}`
+    )
+
+    if (values.title && values.address)
+        postListing(
+            values.title,
+            description.value,
+            price.value,
+            values.address,
+            phonenumber.value
+        )
+})
+</script>
