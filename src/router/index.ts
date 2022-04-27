a62367ab8868f8e9ac695cd5d1ee58ed1235d553
Vilde Gylterud <vildegy@stud.ntnu.no>
2022-04-27 17:43:03 +0200

    Accept borrow request and search-button in landingpage

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import EditUserView from "../views/EditUserView.vue"
 import CreateUserView from "../views/CreateUserView.vue"
 import AllListingView from "../views/AllListingView.vue"
 import CreateListingView from "../views/CreateListingView.vue"
+import AcceptBorrowRequest from "../views/AcceptBorrowRequest.vue"
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -47,7 +48,16 @@ const routes: Array<RouteRecordRaw> = [
         component: () =>
             import(/* webpackChunkName: "faq" */ "../views/FaqView.vue"),
     },
-
+    {
+        path: "/acceptBorrowRequest",
+        name: "acceptBorrowRequest",
+        component: AcceptBorrowRequest,
+    },
+    {
+        path: "/search/:searchstring",
+        name: "searchlisting",
+        component: LandingPage,
+    },
     {
         path: "/listingView",
         name: "AllListingView",
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -11,51 +11,55 @@
                     max-width="200"
                 ></v-img>
             </div>
-            <div>
-                <div
-                    class="ma-10 d-flex align-center text-center justify-center"
-                >
-                    <v-col cols="10">
-                        <h2 class="text-h6 font-italic">
-                            "For å skape et bærekraftig samfunn"
-                        </h2>
-                    </v-col>
-                </div>
-                <div id="inputAndButton">
-                    <v-col cols="12">
-                        <div
-                            class="d-flex align-center text-center justify-center"
-                        >
-                            <v-text-field
-                                label="Hva ønsker du å leie?"
-                                placeholder="Hva ønsker du å leie?"
-                                variant="outlined"
-                                class="white ma-0 pa-0"
-                                color="#004aad"
-                                bg-color="white"
-                            ></v-text-field>
-                        </div>
-                        <div
-                            class="d-flex align-center text-center justify-center"
-                        >
-                            <v-btn
-                                class="v-btn text-white pa-6"
-                                color="#004aad"
-                                block
-                            >
-                                ...eller leie ut?
-                            </v-btn>
-                        </div>
-                    </v-col>
-                </div>
+            <div class="ma-10 d-flex align-center text-center justify-center">
+                <v-col cols="10">
+                    <h2 class="text-h6 font-italic">
+                        "For å skape et bærekraftig samfunn"
+                    </h2>
+                </v-col>
             </div>
+            <v-col cols="12">
+                <div class="d-flex align-center text-center justify-center">
+                    <v-text-field
+                        label="Hva ønsker du å leie?"
+                        placeholder="Hva ønsker du å leie?"
+                        variant="outlined"
+                        class="white"
+                        color="#004aad"
+                        bg-color="white"
+                        v-model="searchbar"
+                    >
+                    </v-text-field>
+                    <v-btn
+                        class="text-white mx-2 mb-10"
+                        height="52"
+                        width="80"
+                        color="#004aad"
+                        @click="search"
+                    >
+                        <v-icon size="25"> mdi-magnify </v-icon>
+                    </v-btn>
+                </div>
+                <div class="d-flex align-center text-center justify-center">
+                    <v-btn class="v-btn text-white pa-6" color="#004aad" block>
+                        ...eller leie ut?
+                    </v-btn>
+                </div>
+            </v-col>
         </v-parallax>
     </v-container>
 </template>
 
-<script>
-export default {
-    name: "LandingPage",
+<script setup lang="ts">
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+const searchbar = ref("")
+const router = useRouter()
+const search = () => {
+    router.push({
+        name: "searchlisting",
+        params: { searchstring: searchbar.value },
+    })
 }
 </script>
 
