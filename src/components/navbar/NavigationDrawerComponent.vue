8c5188947aefcf4f5ca782317e9a65233fcda7e2
Ida Angell Veglo Klæstad <iaklaest@stud.ntnu.no>
2022-05-03 15:01:41 +0200

    feat(Notification): Made the design and logic for Notification

--- src/components/navbar/NavigationDrawerComponent.vue
+++ src/components/navbar/NavigationDrawerComponent.vue
@@ -26,37 +26,59 @@
 
         <v-divider />
 
-        <v-list nav>
-            <v-list-item
-                prepend-icon="mdi-magnify"
-                title="Finn noe å leie"
-                @click="$router.push({ name: 'landingpage' })"
-            />
+        <div v-if="!chat">
+            <v-list nav>
+                <v-list-item
+                    prepend-icon="mdi-magnify"
+                    title="Finn noe å leie"
+                    @click="$router.push({ name: 'landingpage' })"
+                />
 
-            <v-list-item
-                prepend-icon="mdi-animation"
-                title="Alle annonser"
-                @click="$router.push({ name: 'alllistingview' })"
-            />
+                <v-list-item
+                    prepend-icon="mdi-animation"
+                    title="Alle annonser"
+                    @click="$router.push({ name: 'alllistingview' })"
+                />
 
-            <v-list-item
-                prepend-icon="mdi-animation"
-                title="Mine annonser"
-                @click="$router.push({ name: 'personallistingview' })"
-            />
+                <v-list-item
+                    prepend-icon="mdi-animation"
+                    title="Mine annonser"
+                    @click="$router.push({ name: 'personallistingview' })"
+                />
 
-            <v-list-item
-                prepend-icon="mdi-plus"
-                title="Opprett annonse"
-                @click="$router.push({ name: 'createlisting' })"
-            />
+                <v-list-item
+                    prepend-icon="mdi-plus"
+                    title="Opprett annonse"
+                    @click="$router.push({ name: 'createlisting' })"
+                />
 
-            <v-list-item
-                prepend-icon="mdi-help-circle-outline"
-                title="Ofte stilte spørsmål"
-                @click="$router.push({ name: 'faq' })"
-            />
-        </v-list>
+                <v-list-item
+                    prepend-icon="mdi-chat"
+                    title="Chat"
+                    @click="chat = !chat"
+                    ><div v-if="unread">
+                        <v-badge dot color="red" inline /></div
+                ></v-list-item>
+
+                <v-list-item
+                    prepend-icon="mdi-help-circle-outline"
+                    title="Ofte stilte spørsmål"
+                    @click="$router.push({ name: 'faq' })"
+                />
+            </v-list>
+        </div>
+        <div v-else>
+            <v-list nav>
+                <v-list-item
+                    prepend-icon="mdi-arrow-collapse-left"
+                    title="Back"
+                    @click="chat = !chat"
+                />
+                <v-list-item>
+                    <NotificationCardComponent />
+                </v-list-item>
+            </v-list>
+        </div>
     </v-navigation-drawer>
 
     <!--Navigation bar-->
@@ -71,6 +93,9 @@
             @click="isNavigationOpen = !isNavigationOpen"
             data-test-id="hamburger-menu-button"
         ></v-app-bar-nav-icon>
+        <div v-if="unread">
+            <v-badge dot color="red" class="notification" />
+        </div>
 
         <v-img
             src="@/assets/navbar-logo.png"
@@ -108,6 +133,8 @@ import { ref, defineProps } from "vue"
 import { useStore } from "vuex"
 import LoginPopupComponent from "@/components/user/LoginPopupComponent.vue"
 import { getUser } from "@/services/api/user"
+import NotificationCardComponent from "@/components/notifications/NotificationCardComponent.vue"
+import { getNotifications } from "@/services/api/notification"
 
 const store = useStore()
 const props = defineProps({
@@ -120,6 +147,8 @@ const isNavigationOpen = ref(true)
 const isAuthenticated = ref(false)
 const fullName = ref("")
 const email = ref("")
+const chat = ref(false)
+const unread = ref(true)
 
 const updateLoginState = async () => {
     isAuthenticated.value = store.getters.isLoggedIn
@@ -135,7 +164,18 @@ const updateLoginState = async () => {
     }
 }
 
-updateLoginState()
+const token = store.getters.token
+getNotifications(token).then((data) => {
+    unread.value = data.unread
+})
 
+updateLoginState()
 const avatar = ref(require("@/assets/user-avatar-placeholder.png"))
 </script>
+
+<style scoped>
+.notification {
+    margin-left: -12px;
+    margin-bottom: 15px;
+}
+</style>
